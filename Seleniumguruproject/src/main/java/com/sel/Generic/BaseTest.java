415383c69b973343614d8f0e8453857f92921a61
fasilnaseeb <56686221+fasilnaseeb@users.noreply.github.com>
2020-11-03 14:17:29 +0530

    Update BaseTest.java
    base test scheema

--- Seleniumguruproject/src/main/java/com/sel/Generic/BaseTest.java
+++ Seleniumguruproject/src/main/java/com/sel/Generic/BaseTest.java
@@ -6,7 +6,52 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class BaseTest implements IAutoConstant {
 
 	
-	WebDriver driver = new ChromeDriver();
+	public static WebDriver driver;
+	public static Properties prop;
+
+	public BaseTest() {
+		try
+
+		{
+			prop = new Properties();
+			FileInputStream ip = new FileInputStream(PROP_PATH);
+			prop.load(ip);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
-	driver.get("");
+	public static void initilization() {
+		String browserName = prop.getProperty("browser");
+		if (browserName.equals("chrome")) {
+			System.setProperty(CHROME_KEY, CHROME_VALUE);
+			driver = new ChromeDriver();
+
+		}
+
+		else if (browserName.equals("firefox")) {
+
+			System.setProperty(GECKO_KEY, GECKO_VALUE);
+			driver = new FirefoxDriver();
+
+		}
+
+		else 
+		{
+			System.out.println("Invalid browser");
+		}
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(IMPLICIT_WAIT, TimeUnit.SECONDS);
+		
+	    driver.get(prop.getProperty("url"));
+
+	}
+
+	
+	
 }
